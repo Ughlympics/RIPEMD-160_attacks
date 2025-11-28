cd5ff095106f0cc363ec537c36bd52f792a2b1bc
Ughlympics <kipgora@gmail.com>
2025-11-28 02:23:15 +0200

    Lab1 done

--- attacks/attack.go
+++ attacks/attack.go
@@ -2,39 +2,45 @@ package attacks
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 	"time"
 
 	"golang.org/x/crypto/ripemd160"
 )
 
-func Attack1v1() bool {
-	res := false
-	msg := []byte("RadkevichKyrylMykolayovich")
+func Attack1v1(s string) int {
+	msg := []byte(s)
 
 	h := ripemd160.New()
 	h.Write(msg)
 	var pib = h.Sum(nil)
+
 	target1 := pib[len(pib)-2]
 	target2 := pib[len(pib)-1]
 
-	for i := 0; res == false || i < 2^17; i++ {
+	for i := 0; i < (1 << 18); i++ {
 		str := fmt.Sprintf("%d", i)
-		msg := []byte("RadkevichKyrylMykolayovich" + str)
+		msg := []byte(s + str)
 
 		h2 := ripemd160.New()
 		h2.Write(msg)
 		sum := h2.Sum(nil)
 
+		// if i < 30 {
+		// 	fmt.Printf("Trying suffix: +%s => hash: %x\n", str, sum)
+		// }
+
 		if sum[len(sum)-2] == target1 && sum[len(sum)-1] == target2 {
 			fmt.Printf("Found collision suffix: +%s\n", str)
 			fmt.Printf("Colliding hash: %x\n", sum)
-			res = true
+			fmt.Printf("count i: %x\n", i)
+			return i
 		}
 	}
 
 	fmt.Printf("RIPEMD-160: %x\n", pib)
-	return res
+	return -1
 }
 
 func randomModify(s string) string {
@@ -71,9 +77,8 @@ func randomModify(s string) string {
 	return string(runes)
 }
 
-func Attack1v2() bool {
-	res := false
-	msg := []byte("RadkevichKyrylMykolayovich")
+func Attack1v2(s string) int {
+	msg := []byte(s)
 
 	h := ripemd160.New()
 	h.Write(msg)
@@ -81,26 +86,31 @@ func Attack1v2() bool {
 	target1 := pib[len(pib)-2]
 	target2 := pib[len(pib)-1]
 
-	for i := 0; res == false || i < 2^17; i++ {
-		str := fmt.Sprintf("%d", i)
+	for i := 0; i < (1 << 18); i++ {
+		//str := fmt.Sprintf("%d", i)
 		msg := []byte(randomModify("RadkevichKyrylMykolayovich"))
 
 		h2 := ripemd160.New()
 		h2.Write(msg)
 		sum := h2.Sum(nil)
 
+		// if i < 30 {
+		// 	fmt.Printf("Trying suffix: +%s => hash: %x\n", str, sum)
+		// }
+
 		if sum[len(sum)-2] == target1 && sum[len(sum)-1] == target2 {
-			fmt.Printf("Found collision suffix: +%s\n", str)
-			fmt.Printf("Colliding hash: %x\n", sum)
-			res = true
+			//fmt.Printf("Found collision suffix: +%s\n", str)
+			//fmt.Printf("Colliding hash: %x\n", sum)
+			return i
+
 		}
 	}
 
-	return res
+	return -1
 }
 
-func Attack2v1() bool {
-	base := "RadkevichKyrylMykolayovich"
+func Attack2v1(s string) int {
+	base := s
 
 	seen := make(map[[4]byte]string)
 
@@ -119,6 +129,10 @@ func Attack2v1() bool {
 			sum[len(sum)-1],
 		}
 
+		// if i < 30 {
+		// 	fmt.Printf("Trying suffix: +%s => hash last 4 bytes: %x\n", suffix, last4)
+		// }
+
 		if prev, ok := seen[last4]; ok {
 			fmt.Println("=====================================")
 			fmt.Println("COLLISION FOUND!")
@@ -126,12 +140,115 @@ func Attack2v1() bool {
 			fmt.Printf("String 2: %s\n", base+suffix)
 			fmt.Printf("Hash last 4 bytes: %x\n", last4)
 			fmt.Println("=====================================")
-			return true
+			return i
 		}
 
 		seen[last4] = base + suffix
 	}
 
 	fmt.Println("No collisions found.")
-	return false
+	return -1
+}
+
+func Attack2v2(s string) int {
+	base := s
+
+	h := ripemd160.New()
+	h.Write([]byte(base))
+	baseHash := h.Sum(nil)
+
+	target := [4]byte{
+		baseHash[len(baseHash)-4],
+		baseHash[len(baseHash)-3],
+		baseHash[len(baseHash)-2],
+		baseHash[len(baseHash)-1],
+	}
+
+	seen := make(map[[4]byte]string)
+
+	seen[target] = base
+
+	for i := 0; i < (1 << 17); i++ {
+
+		msg := randomModify(base)
+
+		if msg == base {
+			continue
+		}
+
+		h2 := ripemd160.New()
+		h2.Write([]byte(msg))
+		sum := h2.Sum(nil)
+
+		last4 := [4]byte{
+			sum[len(sum)-4],
+			sum[len(sum)-3],
+			sum[len(sum)-2],
+			sum[len(sum)-1],
+		}
+
+		// if i < 30 {
+		// 	fmt.Printf("Trying message: %s => hash last 4 bytes: %x\n", msg, last4)
+		// }
+
+		if prevMsg, ok := seen[last4]; ok {
+			if prevMsg != msg {
+				fmt.Println("=====================================")
+				fmt.Println("COLLISION FOUND!")
+				fmt.Printf("Message 1: %s\n", prevMsg)
+				fmt.Printf("Message 2: %s\n", msg)
+				fmt.Printf("Hash last 4 bytes: %x\n", last4)
+				fmt.Println("=====================================")
+				return i
+			}
+		}
+
+		seen[last4] = msg
+	}
+
+	fmt.Println("No collision found.")
+	return -1
+}
+
+func RunAttackStats() {
+	const runs = 100
+	iterations := make([]float64, 0, runs)
+
+	for i := 0; i < runs; i++ {
+		k := i * 6
+		pib := "RadkevichKyrylMykolayovich" + fmt.Sprintf("%d", k)
+		fmt.Printf("Run %d/%d: attacking pib='%s'\n", i+1, runs, pib)
+
+		//pib := "RadkevichKyrylMykolayovich"
+
+		it := Attack2v2(pib)
+		if it != -1 {
+			iterations = append(iterations, float64(it))
+		}
+	}
+
+	var sum float64
+	for _, v := range iterations {
+		sum += v
+	}
+	m_expectation := sum / float64(len(iterations))
+
+	var diffsq float64
+	for _, v := range iterations {
+		diffsq += (v - m_expectation) * (v - m_expectation)
+	}
+	variance := diffsq / float64(len(iterations))
+	stddev := math.Sqrt(variance)
+
+	n := float64(len(iterations))
+	ciLow := m_expectation - 1.96*(stddev/math.Sqrt(n))
+	ciHigh := m_expectation + 1.96*(stddev/math.Sqrt(n))
+
+	fmt.Println("============ STATISTICS ============")
+	fmt.Printf("Runs:                %d\n", len(iterations))
+	fmt.Printf("MATH EXPECTATION:    %.2f\n", m_expectation)
+	fmt.Printf("Dispersion:          %.2f\n", variance)
+	fmt.Printf("Std deviation:       %.2f\n", stddev)
+	fmt.Printf("95%% CI:              [%.2f ; %.2f]\n", ciLow, ciHigh)
+	fmt.Println("====================================")
 }
--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
-	//"RIPEMD/attacks"
 	"RIPEMD/attacks"
 	"fmt"
+
+	"golang.org/x/crypto/ripemd160"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
-	attacks.Attack1v1()
-	attacks.Attack1v2()
-	attacks.Attack2v1()
+	fmt.Println("Starting RIPEMD-160 attacks...")
+	var pib = "RadkevichKyrylMykolayovich"
+	msg := []byte(pib)
+
+	h := ripemd160.New()
+	h.Write(msg)
+	fmt.Printf("RIPEMD-160 of original message(hash of %s): %x\n", msg, h.Sum(nil))
+	//attacks.Attack1v1(pib)
+	//attacks.Attack1v2(pib)
+	//attacks.Attack2v1(pib)
+	//attacks.Attack2v2(pib)
+	attacks.RunAttackStats()
 
 }
